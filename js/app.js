68c056d49797e8653f7f9cada76666f005a5e3a0
trvrcallaway <trevor@stjoeweb.com>
2015-01-03 17:15:15 -0600

    sf 1/3

--- js/app.js
+++ js/app.js
@@ -8,13 +8,27 @@ $(document).ready(function(){
     $('.ryu-still').show();
   })
   .mousedown(function() {
-    // play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-  })
+    $('.hadouken').finish().show()
+    .animate(
+    {'left': '900px'},
+    500,
+    function() {
+      $(this).hide();
+      $(this).css('left', '500px');
+    }
+  );
+})
   .mouseup(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
 });
+
+function playHadouken () {
+  $('#hadouken-sound') {0} .volume = 0.5;
+  $('#hadouken-sound') {0} .load();
+  $('#hadouken-sound') {0} .play();
+}
